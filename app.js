f25439f28d79e9965539b48546023048ddb4bf07
refnaldy <refnaldyhartono7x7@gmail.com>
2022-05-27 04:35:12 +0700

    done2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Controller = require('./controllers/controller');
 const session = require('express-session');
 const { is } = require('express/lib/request');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
